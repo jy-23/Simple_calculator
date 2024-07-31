91bb4d5e1b523548134fdc2e355dbf650fd42b4a
jy-23 <124462309+jy-23@users.noreply.github.com>
2024-07-31 11:14:29 -0700

    Initial commit w/ basic html and functions
    Created the add, subtract, multiply, and divide functions. Along with
    the variables needed as parameters for those functions. Basic calculator
    display as well as the calculator buttons were created using html.

---
+++ script.js
@@ -0,0 +1,22 @@
+// create needed variables
+let firstNum;
+let secondNum;
+let operator;
+
+// basic math functions
+function add(num1,num2) {
+    return num1 + num2;
+}
+
+function subtract(num1,num2) {
+    return num1 - num2;
+}
+
+function multipy(num1,num2) {
+    return num1*num2;
+}
+
+function divide(num1,num2) {
+    return num1 / num2;
+}
+
